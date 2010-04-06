c178c34a613f396a4f9dc260bfa74a76eb1be2b1
Kevin Carter <cartkev@gmail.com>
2010-04-05 18:27:28 -0600

    Add indent dir to symlink scripts

--- vim/clear_symlinks.sh
+++ vim/clear_symlinks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find autoload colors doc ftdetect ftplugin plugin syntax -type l | xargs ls -l | grep external | awk '{print $8}' | xargs rm
+find autoload colors doc ftdetect ftplugin indent plugin syntax -type l | xargs ls -l | grep external | awk '{print $8}' | xargs rm
